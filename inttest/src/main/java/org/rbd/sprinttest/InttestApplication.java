17d7670b237c89be2d104cda8430755258112b7f
randeepbydesign <randeepbydesign@gmail.com>
2018-10-04 17:52:52 -0400

    Adding "client-x"

---
+++ inttest/src/main/java/org/rbd/sprinttest/InttestApplication.java
@@ -0,0 +1,60 @@
+package org.rbd.sprinttest;
+
+import java.io.File;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.filters.SimplePatternFileListFilter;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+@SpringBootApplication
+@Configuration
+@EnableIntegration
+public class InttestApplication {
+
+
+    private static final String INPUT_DIR = "c:\\temp\\springtest\\input";
+    public final String OUTPUT_DIR = "c:\\temp\\springtest\\output";
+	private static final String FILE_PATTERN = "*.txt";
+
+    @Bean
+    public MessageChannel fileChannel() {
+        return new DirectChannel();
+    }
+    
+	@Bean
+    @InboundChannelAdapter(value = "fileChannel", poller = @Poller(fixedDelay = "1000"))
+    public MessageSource<File> fileReadingMessageSource() {
+        FileReadingMessageSource sourceReader= new FileReadingMessageSource();
+        sourceReader.setDirectory(new File(INPUT_DIR));
+        sourceReader.setFilter(new SimplePatternFileListFilter(FILE_PATTERN));
+        return sourceReader;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileChannel")
+    public MessageHandler fileWritingMessageHandler() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(OUTPUT_DIR));
+        handler.setFileExistsMode(FileExistsMode.REPLACE);
+        handler.setExpectReply(false);
+        return handler;
+    }
+    
+	public static void main(String[] args) {
+        System.out.println("Startin spring!");
+        SpringApplication.run(InttestApplication.class, args);
+        System.out.println("Bailing!");
+	}
+}
